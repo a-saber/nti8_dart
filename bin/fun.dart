70bcbc9aaec6b79931b3b37efdc08040a72d869c
a-saber <ahs629862@gmail.com>
2026-06-14 21:17:26 +0300

    fun 01 task

---
+++ bin/fun.dart
@@ -0,0 +1,29 @@
+void main(){
+  int result = calculate(10, 20);
+  print(result);
+}
+
+// Build
+// header
+// ReturnType funName (Params)
+// Body
+//{}
+
+int calculate(int x, int y)
+{
+  int sum = x + y;
+  return sum;
+}
+
+/*
+build function 
+Map<String, dynamic> productAsMap = buildMap("Product A", 100, "Apple", 10);
+print(productAsMap);
+{
+  'name': 'Product A',
+  'price': 100,
+  'brand': 'Apple',
+  'quantity': 10,
+  "in_stock": true
+}
+*/
