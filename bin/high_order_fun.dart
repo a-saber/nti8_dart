88b28b90e3d2a50f100c2e8cd7e61a696e900861
a-saber <ahs629862@gmail.com>
2026-06-19 17:25:04 +0300

    high order fun task 01

--- bin/high_order_fun.dart
+++ bin/high_order_fun.dart
@@ -1,3 +1,34 @@
+int calculateTotal(int Function(int price) applyDiscount){
+  return applyDiscount(100);
+}
+
+/*
+calculateTotal(applyDiscount)
+
+
+void main(){
+  calculateTotal(withPromoCode);
+  calculateTotal(withCoupon);
+  calculateTotal(withVoucher);
+
+}
+
+withPromoCode(int price){
+  return price - 10;
+}
+
+withCoupon(int price){
+  return price - 5;
+}
+
+withVoucher(int price){
+  return price - 15;
+}
+
+ */
+
+
+
 
 int sum(int s1, int s2){
   return s1 + s2;
