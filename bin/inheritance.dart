8fbcaec9522c0d58a704c2124816a939158790ac
a-saber <ahs629862@gmail.com>
2026-06-20 17:35:50 +0300

    oop task 04 inheritance

---
+++ bin/inheritance.dart
@@ -0,0 +1,62 @@
+class Person{
+  String? name;
+  int? age;
+
+  Person(this.name, this.age);
+
+  void displayInfo(){
+    print('Name: $name');
+    print('Age: $age');
+  }
+}
+
+class Employee extends Person {
+
+  String? job;
+  Employee(super.name, super.age, {required this.job, });
+
+  @override
+  void displayInfo(){
+    super.displayInfo();
+    print('job: $job');
+
+  }
+}
+
+class Manager extends Employee{
+  String? department;
+  Manager(super.name, super.age, {super.job, this.department});
+
+  @override
+  void displayInfo(){
+    print('department: $department');
+    super.displayInfo();
+  }
+}
+
+
+void main(){
+
+  Manager e = Manager('ahmed', 20, job: 'flutter developer', department: 'S');
+  e.displayInfo();
+}
+
+/*
+
+Vehicle
+- brand
+- model
+- displayInfo()
+Vehicle() : Positional Parameters
+
+Car
+- color
+- doorsNumber
+- displayInfo()
+Car() : Named Parameters
+
+ElectricCar
+- batteryCapacity
+- displayInfo()
+ElectricCar() : Named Parameters
+ */
