e28853399da3c3c780c5b9ab9d0f6c18ff8ff928
a-saber <ahs629862@gmail.com>
2026-06-20 18:47:36 +0300

    oop task 05 abstract +

---
+++ bin/abstract.dart
@@ -0,0 +1,80 @@
+abstract class Vehicle{
+
+  displayInfo(){
+    print('Vehicle');
+  }
+  void startEngine();
+}
+
+class Cycle implements Vehicle{
+
+  @override
+  void startEngine() {
+
+  }
+
+  @override
+  void displayInfo() {
+
+  }
+}
+
+class Car extends Vehicle{
+  @override
+  void startEngine() {
+    print('Engine started');
+  }
+
+  @override
+  void displayInfo() {
+    print('Car');
+    super.displayInfo();
+  }
+}
+
+abstract class Bus extends Vehicle{
+
+  void busStart();
+
+  void busInfo(){}
+
+}
+
+class BusA extends Bus{
+  @override
+  void busStart() {
+    print('Bus started');
+  }
+
+  @override
+  void startEngine() {
+    print('Engine started');
+  }
+}
+
+/*
+
+Employee
+- name
+- salary
+displayInfo() HAS BODY
+calculateBonus() NO BODY
+
+Manager extends Employee
+- department
+displayInfo() HAS BODY
+calculateBonus() HAS BODY
+
+Engineer extends Employee
+- specialization
+displayInfo() HAS BODY
+calculateBonus() NO BODY
+
+Developer extends Engineer
+- programmingLanguage
+displayInfo() HAS BODY
+calculateBonus() HAS BODY
+
+
+
+ */
--- bin/inheritance.dart
+++ bin/inheritance.dart
@@ -37,8 +37,24 @@ class Manager extends Employee{
 
 void main(){
 
-  Manager e = Manager('ahmed', 20, job: 'flutter developer', department: 'S');
-  e.displayInfo();
+  // Manager e = Manager('ahmed', 20, job: 'flutter developer', department: 'S');
+  // e.displayInfo();
+
+  Vehicle v = Vehicle('brand 1', 'model 1');
+  displayVehicleInfo(v);
+
+  Car c = Car(color: 'Color 2', doorNumbers: 4, model: 'model 2', brand: 'brand 2');
+  displayVehicleInfo(c);
+
+  ElectricCar ec = ElectricCar(color: 'Color 3', doorNumbers: 5, model: 'model 3', brand: 'brand 3', batteryCapacity: 100);
+  displayVehicleInfo(ec);
+
+  displayVehicleInfo(ElectricCar(color: 'Color 3', doorNumbers: 5, model: 'model 3', brand: 'brand 3', batteryCapacity: 100));
+
+}
+
+void displayVehicleInfo(Vehicle v){
+  v.displayInfo();
 }
 
 /*
@@ -62,3 +78,76 @@ ElectricCar
 - startEngine() => "Engine started Silently"
 ElectricCar() : Named Parameters
  */
+
+
+
+
+
+
+class Vehicle{
+
+  String? brand;
+  String? model;
+
+  Vehicle(this.brand, this.model);
+  void displayInfo(){
+    print('brand: $brand');
+    print('model: $model');
+  }
+}
+
+class Car extends Vehicle{
+  String? color;
+  int? doorNumbers;
+  Car({
+    required this.color,
+    required this.doorNumbers,
+    required String? model,
+    required String? brand
+}): super(brand, model);
+
+  Car.brandA(
+  super.brand,
+  super.model,
+  {
+    required this.color,
+    required this.doorNumbers
+
+  });
+
+  @override
+  void displayInfo(){
+    super.displayInfo();
+    print('color: $color');
+    print('doorNumbers: $doorNumbers');
+  }
+
+  void startEngine()=> print('startEngine');
+}
+
+class ElectricCar extends Car{
+  int? batteryCapacity;
+  ElectricCar({
+    required this.batteryCapacity,
+    required super.color,
+    required super.doorNumbers,
+    required super.brand,
+    required super.model
+});
+  ElectricCar.brandA(
+      String? model,
+      String? brand,
+      String? color,
+      int? doorNumbers
+      ):
+      super.brandA(brand, model, color: color, doorNumbers: doorNumbers);
+
+  @override
+  void displayInfo(){
+    super.displayInfo();
+    print('batteryCapacity: $batteryCapacity');
+  }
+  @override
+  void startEngine()=> print('Engine started Silently');
+}
+
