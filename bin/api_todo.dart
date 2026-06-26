9f98dc533441d524107ebdbb50d58384bfedc2f6
a-saber <ahs629862@gmail.com>
2026-06-26 17:43:13 +0300

    serialize task 01 solve

--- bin/api_todo.dart
+++ bin/api_todo.dart
@@ -11,7 +11,33 @@ void main() async{
 
 
 }
+class LoginResponse{
+  String? accessToken;
+  String? refreshToken;
+  bool? status;
+  User? user;
+  LoginResponse({this.status, this.accessToken, this.refreshToken, this.user});
+  LoginResponse.fromJson(Map<String, dynamic> json){
+    accessToken = json['access_token'];
+    refreshToken = json['refresh_token'];
+    status = json['status'];
+    if(json['user'] != null){
+      user = User.fromJson(json['user']);
+    }
+  }
 
+}
+class User{
+  int? id;
+  String? imagePath;
+  String? username;
+  User({this.id, this.username, this.imagePath});
+  User.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    imagePath = json['image_path'];
+    username = json['username'];
+  }
+}
 Future<void> login() async {
   try{
     var response = await dio.post('login',
@@ -19,8 +45,12 @@ Future<void> login() async {
             {'username': 'ahmed-saber', 'password': '123456'}));
 
     var mapResponse = response.data as Map<String, dynamic>;
-    accessToken = mapResponse['access_token'];
-    refreshToken = mapResponse['refresh_token'];
+    LoginResponse loginResponse =
+    LoginResponse.fromJson(mapResponse);
+
+
+    accessToken = loginResponse.accessToken;
+    refreshToken = loginResponse.refreshToken;
 
     print(mapResponse.toString());
   }
--- bin/serialize.dart
+++ bin/serialize.dart
@@ -1,5 +1,5 @@
-
 Map<String, dynamic> countries = {
+  'status': true,
   'countries': [
     {
       'id': 1,
@@ -9,14 +9,9 @@ Map<String, dynamic> countries = {
         {
           'id': 2,
           'name': 'Cairo',
-          'area': 10000
+          'land_area': 10000,
         },
-        {
-          'id': 4,
-          'name': 'Alexandria',
-          'area': 5000
-        }
-
+        {'id': 4, 'name': 'Alexandria', 'land_area': 5000},
       ]
     },
     {
@@ -24,20 +19,15 @@ Map<String, dynamic> countries = {
       'name': 'United-States',
       'language': 'en',
       'cities': [
-        {
-          'id': 10,
-          'name': 'London',
-          'area': 2000
-        }
+        {'id': 10, 'name': 'London', 'land_area': 2000}
       ]
     }
   ]
 };
 
-
 void main() {
   List<int> numbers = [];
-  numbers.forEach((l){});
+  numbers.forEach((l) {});
   Map<String, dynamic> usersResponse = {
     'status': true,
     'users': [
@@ -47,40 +37,38 @@ void main() {
         'age': 30,
         'isMale': true,
       },
-      {
-        'name': 'Mo',
-        'email': 'mo@gmail.com',
-        'age': 25,
-        'isMale': true
-      },
+      {'name': 'Mo', 'email': 'mo@gmail.com', 'age': 25, 'isMale': true},
     ]
   };
-  GetUsersResponse getUsersResponse =
-      GetUsersResponse.fromJson(usersResponse);
+  GetUsersResponse getUsersResponse = GetUsersResponse.fromJson(usersResponse);
   print(getUsersResponse.users?.first.name);
   print(getUsersResponse.users?.length);
   // print(userMap['name']);
   // User u1 = User.fromJson(userMap);
   // print(u1.name); // 'ahmed'
+
+
+  GetUsersResponse g2 = GetUsersResponse(users: [
+    User(name: 'ahmed')
+  ]);
 }
 
 class GetUsersResponse {
   bool? status;
   List<User>? users;
+
   GetUsersResponse({this.status, this.users});
 
-  GetUsersResponse.fromJson(Map<String, dynamic> json){
+  GetUsersResponse.fromJson(Map<String, dynamic> json) {
     status = json['status'];
-    if(json['users'] != null) {
+    if (json['users'] != null) {
       users = [];
-      json['users'].forEach(
-              (userMap) {
-            User u = User.fromJson(userMap);
-            users!.add(u);
-          });
+      json['users'].forEach((userMap) {
+        User u = User.fromJson(userMap);
+        users!.add(u);
+      });
     }
   }
-
 }
 
 class User {
@@ -96,5 +84,55 @@ class User {
     email = json['email'];
     age = json['age'];
     isMale = json['isMale'];
+  }
+}
+
+class City {
+  int? id;
+  String? name;
+  int? landArea;
+
+  City({this.id, this.name, this.landArea});
+
+  City.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    landArea = json['land_area'];
+  }
+}
+
+class Country {
+  int? id;
+  String? name;
+  String? language;
+  List<City>? cities;
+
+  Country({this.name, this.id, this.language, this.cities});
+
+  Country.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    language = json['language'];
+    if (json['cities'] != null) {
+      cities = [];
+      json['cities'].forEach((cityMap) => cities!.add(City.fromJson(cityMap)));
+    }
+  }
+}
+
+class CountriesResponse {
+  bool? status;
+  List<Country>? countries;
+
+  CountriesResponse(this.status, this.countries);
+
+  CountriesResponse.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    if (json['countries'] != null) {
+      countries = [];
+      json['countries']
+          .forEach((country) =>
+          countries!.add(Country.fromJson(country)));
+    }
   }
 }
